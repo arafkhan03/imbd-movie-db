1158b4cb8d004c2fe4874e735e4dff9cfa78f5bf
Araf Toa Sanjeed Khan <araf.khan03@gmail.com>
2022-12-24 09:32:19 +0600

    Add files via upload

---
+++ app.r
@@ -0,0 +1,6 @@
+library(shiny)
+source('server.r')
+source('ui.r')
+library(shiny)
+
+shinyApp(ui, server)
---
+++ global.r
@@ -0,0 +1,8 @@
+axis_vars <- c(
+  "Tomato Meter" = "Meter",
+  "Numeric Rating" = "Rating",
+  "Number of reviews" = "Reviews",
+  "Dollars at box office" = "BoxOffice",
+  "Year" = "Year",
+  "Length (minutes)" = "Runtime"
+)
